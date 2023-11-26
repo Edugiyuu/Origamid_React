9623248275285060925d816da6acb6e42104ea12
Eduardo Santos <edupaz077@gmail.com>
2023-11-25 23:20:39 -0300

    Use State tests

--- Use State/src/App.jsx
+++ Use State/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Modal from './Modal';
 import ButtonModal from './ButtonModal';
 
-const App = () => {
+/* const App = () => {
   const [modal, setModal] = React.useState(false);
 
   return (
@@ -11,6 +11,27 @@ const App = () => {
       <ButtonModal setModal={setModal} />
     </div>
   );
+}; */
+
+//mudando o estado
+let ativo = true;
+function Teste(){
+ if (ativo === true) {
+  ativo = false
+ }else{
+  ativo = true
+ }
+ console.log(ativo);
+}
+
+const App = () => {
+  
+  //o ternario não muda porque o react não vai renderizar-lo novamente porque uma variavel mudou..
+  //pra isso que existem os hooks
+  return (
+    <button onClick={Teste}>{ativo ? 'Botão Ativo' : 'Botão Inativo'}</button>
+  );
 };
+
 
 export default App;
