ba4510b591a8c63584a05e3d6edbc44d66f85191
Eduardo Santos <edupaz077@gmail.com>
2023-12-06 23:30:12 -0300

    update

--- Animações/src/App.jsx
+++ Animações/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Produto from './Produto';
+import './App.css'
 
 const App = () => {
   const [ativar, setAtivar] = React.useState(false);
--- Animações/src/Produto.jsx
+++ Animações/src/Produto.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Produto = () => {
     return (
-      <div>
+      <div className="animeLeft">
         <h1>Produto</h1>
         <span>R$ 2000</span>
       </div>
