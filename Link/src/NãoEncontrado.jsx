b621a6b65e3a7d8e8c6479611fdf906e6622afa5
Eduardo Santos <edupaz077@gmail.com>
2023-12-10 19:29:05 -0300

    Link, NavLink

---
+++ Link/src/NãoEncontrado.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NãoEncontrado = () => {
+
+
+  return (
+    <div>
+        <h1>Pagina não encontrada</h1>
+    </div>
+  );
+};
+
+
+
+export default NãoEncontrado;
