52623ade0c7f6d5bc89e9bc24a3b0fbbe6a00994
Eduardo Santos <edupaz077@gmail.com>
2023-12-13 20:45:30 -0300

    update

---
+++ Desafio Router/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+
+
+  return (
+    <div>
+        <h1>Footer</h1>
+    </div>
+  )
+}
+export default Footer
---
+++ Desafio Router/src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { NavLink } from 'react-router-dom'
+
+export const Header = () => {
+
+
+  return (
+    <nav className={styles.header}>
+        <ul>
+            <li>
+                <NavLink className={styles.link} to='/'>Produtos</NavLink>
+            </li>
+            <li>
+                <NavLink className={styles.link} to='contato'>Contato</NavLink>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+export default Header
