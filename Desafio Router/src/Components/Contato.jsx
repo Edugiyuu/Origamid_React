3e199ccf038ebe43d845aa320dd669808bb39a49
Eduardo Santos <edupaz077@gmail.com>
2023-12-15 02:04:33 -0300

    update

--- Desafio Router/src/Components/Contato.jsx
+++ Desafio Router/src/Components/Contato.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import styles from './Contato.module.css'
-import foto from '../img/contato.jpg'
+import React from 'react';
+import styles from './Contato.module.css';
+import foto from '../img/contato.jpg';
+import Head from './Head';
 
 const Contato = () => {
-
-
   return (
-    <section className={styles.contato}>
-      <img src={foto} alt="" />
-
+    <section className={`${styles.contato} animeLeft`}>
+      <Head title='Contato' />
+      <img src={foto} alt="Máquina de escrever" />
       <div>
-         <h1>Entre em contato</h1>
-         <ul>
-          <li>
-            edupaz@gmail.com
-          </li>
-         </ul>
+        <h1>Entre em contato.</h1>
+        <ul className={styles.dados}>
+          <li>andre@origamid.com</li>
+          <li>99999-9999</li>
+          <li>Rua Ali Perto, 999</li>
+        </ul>
       </div>
-       
     </section>
-  )
-}
+  );
+};
 
-export default Contato
+export default Contato;
