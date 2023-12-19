e5f6846e01a071af978157d0779f6938485aa964
Eduardo Santos <edupaz077@gmail.com>
2023-12-19 00:51:27 -0300

    update

---
+++ useReducer/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function reducer(state, action) {
+  switch (action) {
+    case 'aumentar':
+      return state + 1;
+    case 'reduzir':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, 0);
+
+  return (
+    <div>
+      <button onClick={() => dispatch('aumentar')}>+</button>
+      <button onClick={() => dispatch('reduzir')}>-</button>
+      <p>{state}</p>
+    </div>
+  );
+};
+
+
+
+
+export default App;
